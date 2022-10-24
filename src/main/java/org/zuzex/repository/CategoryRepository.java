2b6baec0951b2777738e62a9c2179435ce446214
ntrohimchuk <ntrohimchuk@zuzex.com>
2022-10-24 09:02:46 +0300

    refactor

--- src/main/java/org/zuzex/repository/CategoryRepository.java
+++ src/main/java/org/zuzex/repository/CategoryRepository.java
@@ -9,6 +9,8 @@ import java.util.Optional;
 @ApplicationScoped
 public class CategoryRepository implements PanacheRepository<Category> {
     public Optional<Category> findByName(String name) {
-        return find("name", name).stream().findFirst();
+        return find("name", name)
+                .stream()
+                .findFirst();
     }
 }
